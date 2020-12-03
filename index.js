39c2ea6e9b16269fb268bf3c4a76e1f39065358b
Anthony M <amiranda21241@columbushs.com>
2020-12-03 16:29:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Rover Object Goes Here
+// ======================
+let rover = {
+    direction : "N",
+    x: 0,
+    y: 0,
+    travelLog: []
+  };
+// ======================
+function turnnLeft(rover){
+  console.log("turnLeft was called!");
+}
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+function moveForward(rover){
+  console.log("moveFoward  was called!")
+}
