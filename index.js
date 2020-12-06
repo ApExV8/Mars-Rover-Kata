a9cfbd1a5e1d629c6c7ebd40b1eff91b0d3c00ba
Anthony M <amiranda21241@columbushs.com>
2020-12-06 04:31:59 +0000

    Functions

--- index.js
+++ index.js
@@ -28,9 +28,40 @@ function turnLeft(rover){
 }
 
 function turnRight(rover){
-  console.log("turnRight was called!");
-}
+  console.log("turnRight was called!")
+  switch(rover.direction) {
+    case "N":
+      rover.direction = "W";
+      break;
+    case "E":
+      rover.direction = "N";
+      break;
+    case "S":
+      rover.direction = "E";
+      break;
+    case "W":
+      rover.direction = "S";
+      break;
+    default:
+      console.log("can't turn");
+  }
 
 function moveForward(rover){
   console.log("moveForward was called");
 }
+switch(rover.direction) {
+    case "N":
+      rover.direction = "W";
+      break;
+    case "E":
+      rover.direction = "N";
+      break;
+    case "S":
+      rover.direction = "E";
+      break;
+    case "W":
+      rover.direction = "S";
+      break;
+    default:
+      console.log("can't turn");
+  }
